281b754c2aa996e0615d89c6d9a991755c0b878e
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2024-03-23 13:01:08 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.body.style.backgroundImage = `url(${localStorage.getItem('background_image_url')})`;
-if(localStorage.getItem('background_image_url')){}else{
+if(localStorage.getItem('background_image_url') == null){
     localStorage.setItem('background_image_url',"https://imageslabo.com/wp-content/uploads/2019/05/101_benishidare-sakura_6414-973x1460.jpg");
 }
 function settings_background_image(){
