c07e19f3d745f39c24da0d4ba6caf9ea875a245c
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2024-03-23 19:25:49 +0900

    _

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.body.style.backgroundImage = `url(${localStorage.getItem('background_image_url')})`;
 if(localStorage.getItem('background_image_url') == null || localStorage.getItem('background_image_url') == 'null' || localStorage.getItem('background_image_url') == undefined || localStorage.getItem('background_image_url') == 'undefined'){
-    localStorage.setItem('background_image_url',"https://sousuke-m.github.io/mobile.app/default/background/image/df1.jpg");
+    localStorage.setItem('background_image_url',"https://sousuke-m.github.io/mobile.app/default/background/image/df2.jpg");
     location.reload();
 }
 function settings_background_image(){
-    var image_url = prompt('背景画像のURLを入力してください','https://sousuke-m.github.io/mobile.app/default/background/image/df2.jpg');
+    var image_url = prompt('背景画像のURLを入力してください','https://sousuke-m.github.io/mobile.app/default/background/image/df1.jpg');
     localStorage.setItem('background_image_url',image_url);
     location.reload();
 }
