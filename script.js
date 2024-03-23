a33d97f2a0d5d89fa000bc9e8e82ddf5a0b9a36e
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2024-03-23 12:58:56 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.body.style.backgroundImage = `url(${localStorage.getItem('background_image_url')})`;
+if(localStorage.getItem('background_image_url')){}else{
+    localStorage.setItem('background_image_url',"https://imageslabo.com/wp-content/uploads/2019/05/101_benishidare-sakura_6414-973x1460.jpg");
+}
 function settings_background_image(){
     var image_url = prompt('背景画像のURLを入力してください','https://sousuke-m.github.io/mobile.app/default/background/image/AC11FA81-DAEE-45AA-8BEE-15134D9B6BDF.jpg');
     localStorage.setItem('background_image_url',image_url);
