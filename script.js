81e3fd225d2237a9dd9e394e81e40b37c4a998f8
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2024-03-23 17:19:10 +0900

    _

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.body.style.backgroundImage = `url(${localStorage.getItem('background_image_url')})`;
-if(localStorage.getItem('background_image_url') == null){
-    localStorage.setItem('background_image_url',"https://imageslabo.com/wp-content/uploads/2019/05/101_benishidare-sakura_6414-973x1460.jpg");
+if(localStorage.getItem('background_image_url') == null || localStorage.getItem('background_image_url') == 'null' || localStorage.getItem('background_image_url') == undefined || localStorage.getItem('background_image_url') == 'undefined'){
+    localStorage.setItem('background_image_url',"https://sousuke-m.github.io/mobile.app/default/background/image/AC11FA81-DAEE-45AA-8BEE-15134D9B6BDF.jpg");
+    location.reload();
 }
 function settings_background_image(){
-    var image_url = prompt('背景画像のURLを入力してください','https://sousuke-m.github.io/mobile.app/default/background/image/AC11FA81-DAEE-45AA-8BEE-15134D9B6BDF.jpg');
+    var image_url = prompt('背景画像のURLを入力してください','https://sousuke-m.github.io/mobile.app/default/background/image/df1.jpg');
     localStorage.setItem('background_image_url',image_url);
     location.reload();
 }
